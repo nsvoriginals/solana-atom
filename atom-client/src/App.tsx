999057e77001aba6c2adbacb69d0faadd2884230
Nsvoriginals <123223662+nsvoriginals@users.noreply.github.com>
2025-03-11 18:19:18 +0530

    added all files

--- atom-client/src/App.tsx
+++ atom-client/src/App.tsx
@@ -7,21 +7,14 @@ import { WalletGenerator } from "./pages/WalletGenator";
 import { TokenLaunchpad } from "./pages/TokenLaunchpad";
 import { WalletContext } from "./WalletProvider";
 import { Faucet } from "./pages/SolanaFaucet";
+import { Landing } from "./pages/LandingPage";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark"  >
-      <div className="flex flex-col items-center justify-center min-h-screen font-N">
+      <div className="flex flex-col items-center justify-center min-h-screen ">
         <h1 className="text-2xl font-bold">Theme Toggle Example</h1>
         <ModeToggle />
-<h1 className="font-Nue text-black">hi there</h1>
-        <Dashboard/>
-        <WalletGenerator></WalletGenerator>
-        <WalletContext>
-        <TokenLaunchpad>
-
-</TokenLaunchpad>
-<Faucet/>
-        </WalletContext>
+        <Landing></Landing>
     
       </div>
     </ThemeProvider>
---
+++ atom-client/src/components/custom/Navbar.tsx
@@ -0,0 +1,10 @@
+export const Navbar=()=>{
+    return <div className="max-w-[80%] h-150px flex items-center justify-center font-medium gap-8 rounded-full py-3 px-10 border-2 border-purple-500" >
+       <h1 className="text-3xl">Atom</h1> 
+       <div className="flex items-center justify-between gap-10">
+        <h1>Token Launchpad</h1>
+        <h1>Solana Dashboard</h1>
+        <h1>Wallet Generate</h1>
+        </div>    
+    </div>
+}
